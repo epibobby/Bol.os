f1edefdb21c6aa4b528d630bce90ccd8f1b55d00
Romain Lucereau <romain.lucereau@epitech.eu>
2011-09-19 18:12:47 +0200

    coucou test test

---
+++ test_lucere_r.cpp
@@ -0,0 +1,5 @@
+#include "test_lucere_r.h"
+
+test_lucere_r::test_lucere_r()
+{
+}
---
+++ test_lucere_r.h
@@ -0,0 +1,10 @@
+#ifndef TEST_LUCERE_R_H
+#define TEST_LUCERE_R_H
+
+class test_lucere_r
+{
+public:
+    test_lucere_r();
+};
+
+#endif // TEST_LUCERE_R_H
